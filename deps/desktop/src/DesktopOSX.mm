ed57b00523db6e4c1a0613166e0060c23fe7d4a5
loqur <codeplane@loqur.com>
2012-07-07 11:58:24 +1000

    changes

--- deps/desktop/src/DesktopOSX.mm
+++ deps/desktop/src/DesktopOSX.mm
@@ -11,9 +11,15 @@
 - (id)initWithFrame:(NSRect)frameRect {
   self = [super initWithFrame:frameRect];
   self.autoresizingMask = (NSViewHeightSizable | NSViewWidthSizable);
-  [self setMainFrameURL:@"http://www.google.com"];
   [[[self mainFrame] frameView] setAllowsScrolling:NO];
   [self setUIDelegate:self];
+  WebPreferences* prefs = [self preferences];
+  [prefs setAutosaves:NO];
+  [prefs setJavaEnabled:NO];
+  [prefs setJavaScriptEnabled:YES];
+  [prefs setPlugInsEnabled:NO];
+  [prefs setPrivateBrowsingEnabled:YES];
+  [self setMainFrameURL:@"http://www.google.com"];
   return self;
 }
 - (NSArray *)webView:(WebView *)sender contextMenuItemsForElement:(NSDictionary *)element defaultMenuItems:(NSArray *)defaultMenuItems {
@@ -21,22 +27,25 @@
 }
 @end
 
+static id window;
+
+@interface LoqurWindow : NSWindow {}
+- (id)initWithContentRect:(NSRect)contentRect styleMask:(NSUInteger)windowStyle backing:(NSBackingStoreType)bufferingType defer:(BOOL)deferCreation;
+@end
+
+@implementation LoqurWindow
+- (id)initWithContentRect:(NSRect)contentRect styleMask:(NSUInteger)windowStyle backing:(NSBackingStoreType)bufferingType defer:(BOOL)deferCreation {
+  self = [super initWithContentRect:contentRect styleMask:windowStyle backing:bufferingType defer:deferCreation];
+  return self;
+}
+
+@end
+
 void desktopInit () {
   [NSAutoreleasePool new];
   [NSApplication sharedApplication];
   [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
-  id menubar = [[NSMenu new] autorelease];
-  id appMenuItem = [[NSMenuItem new] autorelease];
-  [menubar addItem:appMenuItem];
-  [NSApp setMainMenu:menubar];
-  id appMenu = [[NSMenu new] autorelease];
-  id appName = @"loqur.";
-  id quitTitle = [@"Quit " stringByAppendingString:appName];
-  id quitMenuItem = [[[NSMenuItem alloc] initWithTitle:quitTitle
-    action:@selector(terminate:) keyEquivalent:@"q"] autorelease];
-  [appMenu addItem:quitMenuItem];
-  [appMenuItem setSubmenu:appMenu];
-  id window = [[[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 800, 600)
+  window = [[[LoqurWindow alloc] initWithContentRect:NSMakeRect(0, 0, 800, 600)
     styleMask:(NSTitledWindowMask | NSClosableWindowMask | NSMiniaturizableWindowMask)
     backing:NSBackingStoreBuffered defer:NO] autorelease];
   [window cascadeTopLeftFromPoint:NSMakePoint(20,20)];
@@ -45,8 +54,21 @@ void desktopInit () {
   webView.autoresizesSubviews = YES;
   [window setContentView:webView];
   [webView release];
-  [window setTitle:appName];
+  [window setTitle:@"loqur."];
   [window makeKeyAndOrderFront:nil];
+  id menubar = [[NSMenu new] autorelease];
+  id appMenuItem = [[NSMenuItem new] autorelease];
+  [menubar addItem:appMenuItem];
+  [NSApp setMainMenu:menubar];
+  id appMenu = [[NSMenu new] autorelease];
+  id quitMenuItem = [[[NSMenuItem alloc] initWithTitle:@"Quit"
+    action:@selector(terminate:) keyEquivalent:@"q"] autorelease];
+  [appMenu addItem:quitMenuItem];
+  id closeMenuItem = [[[NSMenuItem alloc] initWithTitle:@"Close"
+    action:@selector(close) keyEquivalent:@"w"] autorelease];
+  [closeMenuItem setTarget:window];
+  [appMenu addItem:closeMenuItem];
+  [appMenuItem setSubmenu:appMenu];
   [NSApp activateIgnoringOtherApps:YES];
   [NSApp run];
 }
